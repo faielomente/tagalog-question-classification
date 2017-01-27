18453be16fa71315e0cd04423497e8149ee4bc10
faielomente <frlomente@up.edu.ph>
2017-01-27 20:35:24 +0800

    extract per column and store it on a list

--- extraction.py
+++ extraction.py
@@ -1,9 +1,15 @@
 import sys
+import os
 import csv
 
-file = open("/home/faielomente/Downloads/Help a CS Student Graduate.csv")
-csv_f = csv.reader(file)
+def extract():
+	file = open(os.path.abspath("Documents/THESIS FILES/Help a CS Student Graduate.csv"))
+	csv_f = csv.reader(file)
 
-for row in csv_f:
-    for item in range(2, len(row)):
-        print row[item]
+	for row in csv_f:
+		for item in range(2, len(row)):
+			if row[item].count(":") == 0 and len(row[item]) > 2:
+				row_items = (row[item].split('\n'))
+				print (	row_items)
+
+extract()
