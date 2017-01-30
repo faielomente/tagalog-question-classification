51c13f07d4b482e6d3f1d3dc37d93b898ab5c38f
faielomente <frlomete@up.edu.ph>
2017-01-31 04:54:55 +0800

    end

--- extraction.py
+++ extraction.py
@@ -2,14 +2,37 @@ import sys
 import os
 import csv
 
-def extract():
-	file = open(os.path.abspath("Documents/THESIS FILES/Help a CS Student Graduate.csv"))
-	csv_f = csv.reader(file)
+def extract(input, output):
+
+
+	csv_f = csv.reader(input)
+	wr = csv.writer(output, delimiter=',', quoting=csv.QUOTE_ALL)
+
+	wr.writerow(["Questions", "Category"])
 
 	for row in csv_f:
-		for item in range(2, len(row)):
-			if row[item].count(":") == 0 and len(row[item]) > 2:
-				row_items = (row[item].split('\n'))
-				print (	row_items)
+		for column in range(2, len(row)):
+			if row[column].count(":") == 0:
+				row_items = (row[column].split('\n'))
+				print ("row_item: ", row_items)
+				row_items = delete_empty_string(row_items)
+				wr = csv.writer(output, delimiter='\n', quoting=csv.QUOTE_ALL)
+				wr.writerow(row_items)
 
-extract()
+
+def delete_empty_string(my_list):
+	
+
+	for item in my_list:
+		if len(item) == 0 or item == " ":
+			my_list.remove(item)
+
+	return my_list
+
+
+
+input_file = open(os.path.abspath('files/Help a CS Student Graduate.csv'))
+output_file = open(os.path.abspath('files/output.csv'), 'w')
+extract(input_file, output_file)
+input_file.close()
+output_file.close()
