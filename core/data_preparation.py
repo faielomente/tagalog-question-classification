9c69b570e63479ad6cec38cf311d268e8225842d
faienix <faye@modernmachin.es>
2017-10-31 16:05:15 +0800

    Rename from preprocessing

---
+++ core/data_preparation.py
@@ -0,0 +1,239 @@
+import os
+import csv
+import re
+
+from nltk.tokenize import RegexpTokenizer
+
+
+def extract(input, output):
+
+    csv_f = csv.reader(input)
+    wr = csv.writer(output, delimiter=',', quoting=csv.QUOTE_ALL)
+
+    wr.writerow(["Questions", "Category"])
+
+    for row in csv_f:
+        for column in range(2, len(row)):
+            if row[column].count(":") == 0:
+                row_items = (row[column].split('\n'))
+                row_items = delete_empty_string(row_items)
+                print ("row_item: ", row_items)
+                wr = csv.writer(output, delimiter='\n', quoting=csv.QUOTE_ALL)
+                wr.writerow(row_items)
+
+
+def delete_empty_string(my_list):
+    for item in my_list:
+        if len(item) == 0 or item == " ":
+            my_list.remove(item)
+
+        if re.match("^[0-9]", item) is not None:
+            index = my_list.index(item)
+            tmp = item.lstrip('0123456789.- ')
+            my_list.remove(item)
+            my_list.insert(index, tmp)
+            print item
+
+    return my_list
+
+
+def get_tags(tag, level):
+    if level == 1:
+        tag = tag.split("-")[0]
+    elif level == 2:
+        temp = tag.split("-")[1]
+        tag = temp[:2]
+    elif level == 3:
+        temp = tag.split("-")[1]
+        tag = temp[2:]
+
+    return tag
+
+
+def transform(data):
+    dict = {'NN': 0, 'PR': 0, 'DT': 0, 'LM': 0, 'CC': 0, 'VB': 0, 'JJ': 0,
+            'RB': 0, 'CD': 0, 'TS': 0}
+    temp = []
+    ctr = 0
+
+    for sentence in data:
+        for key in sentence:
+            ctr += 1
+            dict[sentence[key]] += 1
+
+        temp.append(dict)
+        dict = {'NN': 0, 'PR': 0, 'DT': 0, 'LM': 0, 'CC': 0, 'VB': 0,
+                'JJ': 0, 'RB': 0, 'CD': 0, 'TS': 0}
+
+    return temp
+
+
+# def to_numerical():
+#     input_file = open(os.path.abspath('files/dataset_pos.out'))
+
+#     sentence_to_object(input_file)
+#     data = []
+
+#     vector = DictVectorizer(sparse=False)
+#     data = sentence_to_object(input_file)
+#     data = transform(data)
+#     # print "To_numerical"
+#     # print data
+#     X = vector.fit_transform(data)
+
+#     print X
+
+
+def category_vector(file):
+    input_file = file
+    csv_f = csv.reader(input_file)
+
+    cat = []
+    column = 0
+    ctr = 0
+
+    category = {'entity': 1, 'abbreviation': 2, 'description': 3, 'human': 4,
+                'location': 5, 'numeric': 6}
+
+    for row in csv_f:
+        ctr += 1
+        if "Questions" not in row:
+            temp = []
+            for column in range(1, 2):
+                if(row[column] != "Category" and (row[column]) > 0):
+                    temp.append(category[row[column].lower()])
+            cat.append(temp)
+
+    input_file.close()
+
+    return cat
+
+
+def count_qmark(file):
+    text = file.readlines()
+
+    for line in text:
+        if line.count("?") > 2:
+            print line
+
+
+def get_wh_question(file):
+    reader = csv.reader(file)
+
+    q_word_tags = {
+                    'alin': 1, 'saan': 2, 'ano': 3, 'kailan': 4, 'paano': 5,
+                    'sino': 6, 'bakit': 7
+                    }
+    q_words = {
+                'aling', 'alin-alin', 'alin-aling', 'saang', 'saan-saan',
+                'nasaan', 'nasaang', 'anong', 'anu-ano', 'anu-anong', 'inaano',
+                'paanong', 'papaano', 'papaanong', 'sinong', 'sinu-sino',
+                'sino-sinong', 'kailang'
+                }
+    vec = []
+
+    for row in reader:
+        sentence = row[0].split(" ")
+        temp = []
+
+        for word in sentence:
+            if word.lower() in q_word_tags:
+                # print word.lower()
+                temp.append(q_word_tags[word.lower()])
+                vec.append(temp)
+                break
+            elif word.lower() in q_words:
+                for key in q_word_tags:
+                    if key in word.lower():
+                        temp.append(q_word_tags[key])
+                        vec.append(temp)
+                        break
+    return vec
+
+
+def tokenize_word_data(file):
+    reader = csv.reader(file)
+
+    tokenizer = RegexpTokenizer(r'\w+')
+    data = []
+    row = 0
+
+    for row in reader:
+        # Escaping the first row because it only contains the column titles
+        if row == 0:
+            row += 1
+        else:
+            data.append(tokenizer.tokenize(row[0]))
+
+    print len(data)
+
+    return data
+
+
+def to_wordpos_dict(file):
+    text = file.readlines()
+    array = list()
+    dict = {}
+    ctr = 0
+
+    for line in text:
+        data = line.split("\t")
+        if data[0] != "?":
+            word = data[0]
+            pos = data[len(data)-1]
+            # pos = get_tags(pos, 1)
+            dict[word] = pos.strip()
+        elif data[0] == '?':
+            array.append(dict)
+            dict = {}
+        ctr += 1
+
+    # print ctr
+    print array
+
+    return array
+
+# def pos_vec(file):
+#     input_file = file
+#     data = []
+
+#     data = to_wordpos_dict(input_file)
+#     print data
+#     # data = transform(data) #don't transfor for csr implementation
+
+#     input_file.close()
+
+#     return data
+
+
+def main():
+    """
+    1. Preprocessing the raw data.
+    """
+    # input_file=open(os.path.abspath('files/Help a CS Student Graduate.csv'))
+    # output_file=open(os.path.abspath('files/cleaning_output.csv'), 'w')
+    # extract(input_file, output_file)
+    # input_file.close()
+    # output_file.close()
+    ########
+
+    """
+    2. Checking the equality of the pos tags, wh-words and category gathered.
+    Total should be 3077.
+    """
+    dataset = 'files/dataset_pos.out'
+    labelled_data = 'files/labelled_data.csv'
+    pos_data = to_wordpos_dict(open(os.path.abspath(dataset)))
+    wh_vector = get_wh_question(open(os.path.abspath(labelled_data)))
+    category = category_vector(open(os.path.abspath(labelled_data)))
+
+    print "Data Length: ", len(pos_data)
+    print "Wh_Question Length: ", len(wh_vector)
+    print "Category Length: ", len(category)
+    ###########
+
+    # tokenize_word_data(open(os.path.abspath('files/labelled_data.csv')))
+
+
+if __name__ == '__main__':
+    main()
---
+++ core/preprocessing_csr.py
@@ -0,0 +1,224 @@
+import os
+import csv
+import math
+
+
+def to_tuples_array(file):
+    text = file.readlines()
+    array = []
+    sentence = []
+    tuple = ()
+    ctr = 0
+
+    for line in text:
+        data = line.split("\t")
+        if data[0] != "?" and len(line.strip()) > 0:
+            word = data[0]
+            pos = data[len(data)-1]
+            pos = get_coarsetag(pos, 1)
+            tuple = (word, pos.strip())
+            sentence.append(tuple)
+        elif data[0] == '?':
+            array.append(sentence)
+            sentence = []
+            tuple = ()
+
+            ctr += 1
+
+            # print ctr
+
+    # for i in range(0, len(array)):
+    #   print array[i]
+
+    return array
+
+
+def get_coarsetag(tag, level):
+
+    if level == 1:
+        tag = tag.split("-")[0]
+    elif level == 2:
+        temp = tag.split("-")[1]
+        tag = temp[:2]
+    elif level == 3:
+        temp = tag.split("-")[1]
+        tag = temp[2:]
+
+    return tag
+
+
+def get_raw(file, column_no):
+    """
+    Get the labelled sentence from the csv file then convert
+    it to array
+    """
+
+    r = csv.reader(file)
+
+    if column_no == 0:
+        text = []
+        for row in r:
+            if row[0] != "Questions":
+                text.append(row[0].strip())
+
+        # print len(text)
+
+        return text
+    elif column_no == 1:
+        cat = []
+        for row in r:
+            if row[1] != "Category":
+                cat.append(row[1])
+
+        # print len(cat)
+
+        return cat
+
+
+def prune(tuple_array, sentences):
+
+    conj = {"sapagkat", "dahil", "dahil sa", "at saka", "at hindi", "ni hindi",
+            "pero", "datapwat", "ngunit", "subalit", "o", "o kaya",
+            "gayon pa man", "gayumpaman", "gayunman", "kaya", "kung kaya't",
+            "kung kaya", "man", "maging", "hindi lamang", "kundi", "bagaman",
+            "bagama't", "kapag", "kasi", "dahilan sa", "gawa ng", "porke",
+            "porke at", "porke't", "pagkat", "kaya", "kaysa", "kahit",
+            "gayong", "kung", "kung gayon", "habang", "nang", "nang sa gayon",
+            "maging", "maliban kung", "palibhasa", "para", "upang", "parang",
+            "pansamantala", "hanggang"
+            }
+
+    q_words = {'aling', 'alin-alin', 'alin-aling', 'saang', 'saan-saan',
+               'nasaan', 'nasaang', 'anong', 'anu-ano', 'anu-anong', 'inaano',
+               'paanong', 'papaano', 'papaanong', 'sinong', 'sinu-sino',
+               'sino-sinong', 'kailang', 'alin', 'saan', 'ano', 'kailan',
+               'paano', 'sino', 'bakit'
+               }
+
+    for i in range(0, len(sentences)):
+        temp = sentences[i].replace('?', '')
+        text = temp.split(" ")
+
+        for word in text:
+            if word.lower() in conj:
+                ind = text.index(word)
+
+                # conjuction should not be too near in the beginning of the
+                # sentence
+                if len(text) >= 5 and ind >= math.ceil(len(text)/2):
+                    tuple_array[i] = tuple_array[i][:ind]
+                elif ind == 0:
+                    for j in range(ind, len(text)):
+                        if text[j].lower() in q_words:
+                            tuple_array[i] = tuple_array[i][j:len(text)]
+                            break
+
+    # Scan each tuple array for arrays not starting with a wh-word
+    for i in range(0, len(tuple_array)):
+        if tuple_array[i][0][0] not in q_words:
+            t = sentences[i].split(" ")
+
+            for j in range(0, len(tuple_array[i])):
+                if t[j].lower() in q_words:
+                    tuple_array[i] = tuple_array[i][j:]
+
+    # for i in range(0, len(tuple_array)):
+    #   if tuple_array[i][0][0].lower() not in q_words:
+    #       print tuple_array[i], sentences[i]
+
+    return tuple_array
+
+
+def get_data(pruned_array, category, dataset):
+
+    array = []
+
+    if dataset == 1:
+        y = int(math.ceil(len(pruned_array)*0.8))
+
+        for i in range(0, y):
+            data = []
+
+            for j in range(0, len(pruned_array[i])):
+                if j == 0:
+                    data.append(pruned_array[i][j][0].lower())
+                else:
+                    data.append(pruned_array[i][j][1].lower())
+
+            data.append(category[i].lower())
+            array.append(data)
+    elif dataset == 2:
+        x = int(len(pruned_array) - math.ceil(len(pruned_array)*0.2))
+
+        for i in range(x, len(pruned_array)):
+            data = []
+
+            for j in range(0, len(pruned_array[i])):
+                if j == 0:
+                    data.append(pruned_array[i][j][0].lower())
+                else:
+                    data.append(pruned_array[i][j][1].lower())
+
+            data.append(category[i].lower())
+            array.append(data)
+    elif dataset == 3:
+        for i in range(0, len(pruned_array)):
+            data = []
+
+            for j in range(0, len(pruned_array[i])):
+                if j == 0:
+                    data.append(pruned_array[i][j][0].lower())
+                else:
+                    data.append(pruned_array[i][j][1].lower())
+
+            data.append(category[i].lower())
+            array.append(data)
+
+    return array
+
+
+def write_to_file(data):
+    output_file = open(os.path.abspath('files/fpFormat.csv'), 'w')
+
+    wr = csv.writer(output_file, quoting=csv.QUOTE_MINIMAL)
+
+    for i in range(0, len(data)):
+        print(data[i])
+        wr.writerow(data[i])
+
+    output_file.close()
+
+
+def format(partitioned=0):
+    input1 = open(os.path.abspath('files/dataset_pos.out'))
+    input2 = open(os.path.abspath('files/labelled_data.csv'))
+    input3 = open(os.path.abspath('files/labelled_data.csv'))
+
+    tup = to_tuples_array(input1)
+    sen = get_raw(input2, 0)
+    cat = get_raw(input3, 1)
+
+    input1.close()
+    input2.close()
+    input3.close()
+
+    pruned_array = prune(tup, sen)
+
+    if partitioned == 1:
+        training = get_data(pruned_array, cat, 1)
+        testing = get_data(pruned_array, cat, 2)
+
+        return training, testing
+    else:
+        fpFormat = get_data(pruned_array, cat, 3)
+
+        return fpFormat
+
+    # print len(pruned_array)
+    # print len(fpFormat)
+
+    # write_to_file(fpFormat)
+
+
+if __name__ == '__main__':
+    format()
